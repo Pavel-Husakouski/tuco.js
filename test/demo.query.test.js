753a3d2d3f75fc9a1ddf6b992ad3051e425e1dcd
Pavel Husakouski <phusakouski@exadel.com>
2014-10-21 14:21:28 +0300

    grammar refactoring, AST

--- test/demo.query.test.js
+++ test/demo.query.test.js
@@ -1,18 +1,22 @@
 /*
-query           > shapedLogic | logic
-shapedLogic	    > shaped | shaped space [Op [space]] shapedLogic | shaped space [Op space] logic
-shaped          > (expr) | verbatim
-logic    		> singleWord | singleWord space [Op [space]] shapedLogic  | singleWord space [Op space] logic
-Op 				> 'AND' | 'OR'
-verbatim        > '"'[ allExceptQuote ]'"'
-singleWord      > nonSpecials {specials nonSpecials}
-nonSpecials     > nonSpecial {nonSpecial}
-specials        > special {special}
-space 			> singleSpace {singleSpace}
+query           > [spaces] , [ shapedLogic | logic] , [spaces] ;
+shapedLogic	    > shaped , [ logicRest ] ;
+logic    		> singleWord , [ logicRest ] ;
+logicRest		> space , [ Op0 , (shapedLogic | Op1) , logic ] ;
+shaped          > ('(' , query , ')') | verbatim ;
+Op1  			> [('AND' | 'OR') , [space]] ;
+Op0 			> [('AND' | 'OR') , space] ;
+verbatim        > '"' , [ allExceptQuote ] , '"' ;
+singleWord      > nonSpecials , {specials , nonSpecials} ;
+nonSpecials     > nonSpecial , {nonSpecial} ;
+specials        > special , {special} ;
+space 			> singleSpace , {singleSpace} ;
 */
 
 var assert = require("assert");
 var tuco = require('../tuco');
+
+eval(tuco.nsImport('tuco'));
 
 function toString(x){ 
 	if(x == null || typeof x == 'string')
@@ -20,7 +24,39 @@ function toString(x){
 	return x.join(''); 
 };
 
-eval(tuco.nsImport('tuco'));
+function toWord(v){
+	return {kind:'word', value:toString(v)};
+}
+
+function toVerbatim(v) {
+	return {kind:'verbatim', value:toString(v)};
+}
+
+function toOperator(v) {
+	return {kind:v == null ? 'AND' : v};
+}
+
+function toRightArg (v){
+	if(v == null)
+		return v;
+
+	var op = v[0];
+
+	op.right = v[1];
+
+	return op; 
+}
+
+function toLeftArg (v){
+	if(v[1] == null)
+		return v[0];
+
+	var op = v[1];
+
+	op.left =  v[0];
+
+	return op;
+}
 
 var spaceConst = ' \u00A0,;\n\r\t';
 var specialConst = '"()';
@@ -33,26 +69,23 @@ function shapedLogic(x) { return shapedLogicImpl(x); };
 function logic(x) { return logicImpl(x); };
 function query(x) { return queryImpl(x); };
 
-var special = rep1(charOneOf(specialConst));
-var space   = rep1(charOneOf(spaceConst));
-var nonSpecial = rep1(charExcept(spaceConst + specialConst));
-var singleWord = all(nonSpecial.map(toString), rep0( special.map(toString), nonSpecial.map(toString))).map(toString);
-var verbatim =   between('"', rep0(charExcept('"')).map(toString), '"');
-var shaped = or(between('(', query, ')'), verbatim);
-
-var op0 = optional(or('AND', 'OR'), optional(space)); // [Op [space]]
-var op1 = optional(or('AND', 'OR'), space) ;          // [Op space]
-
-var logicRest = or(all(space, op0, shapedLogic), all(space, op1, logic));
-var shapedLogicImpl = all(shaped,     optional(logicRest));
-var logicImpl       = all(singleWord, optional(logicRest));
-
-var queryImpl = or(shapedLogic, logic);
+var special 	= rep1(charOneOf(specialConst)).map(toString);
+var space   	= rep1(charOneOf(spaceConst));
+var nonSpecial 	= rep1(charExcept(spaceConst + specialConst)).map(toString);
+var singleWord 	= all(nonSpecial, rep0( special, nonSpecial)).map(toWord);
+var verbatim 	= between('"', rep0(charExcept('"')), '"').map(toVerbatim);
+var shaped 		= or(between('(', query, ')'), verbatim);
+var op0 		= optional(first(or('AND', 'OR'), optional(space))).map(toOperator); 
+var op1 		= optional(first(or('AND', 'OR'), space)).map(toOperator); 
+var logicRest 	= second(space, optional(or( all(op0, shapedLogic), all(op1, logic) ))).map(toRightArg);
+var shapedLogicImpl = all(shaped, optional(logicRest)).map(toLeftArg);
+var logicImpl   = all(singleWord, optional(logicRest)).map(toLeftArg);
+var queryImpl 	= between(optional(space), optional(or(shapedLogic, logic)), optional(space));
 
 describe('query', function() {
-    it("big", function(){
+   	it("big", function(){
 		var result = query('bamboo OR (join AND ")quted test laenin,()!" bbq) AND "" OR ncx AND pgt');
-
+		
 		assert(result.rest == "");
-    });
+   	});
 });
